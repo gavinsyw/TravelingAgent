059ed60f108e942d278f11658194532fc09ad612
lau-gratin <2895930643@qq.com>
2019-05-17 20:00:40 +0800

    重构

---
+++ itinerary/src/main/java/com/example/administrator/checkbox/ResultAcitivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.checkbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_acitivity);
+    }
+}
