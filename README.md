06dcbfc16e85b34b61f236bba0783d6ff82d3fd2
fffffarmer <fffffarmer@gmail.com>
2019-05-18 15:52:33 +0800

    整合dialogplus失败

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ Project of Software Engineering
 * 添加了模块(Modules)文件夹，开始写一些模块
 * 添加了小组作业文档(Documents)文件夹
 * 路线规划功能(LuXianGuiHua)已经可以接入，但是损失函数还在调试中
+* 目前的项目结构大概为：主模块(app)， PileLayout模块(library)， dialogplus模块(dialogplus)。其中dialogplus尚未完全整合到整个项目中(简直心累orz)
 
 ## Developer Guide
-# 路线规划功能的开发者帮助
+### 路线规划功能的开发者帮助
 * 路线规划功能现在能够再指定酒店的情况下对旅游景点的顺序做10天的规划，可以设置的参数有：一天途径的旅游景点的数量（不能超过6）
+
 * 路线规划功能包中包含关键类Sight，后续开发用到景点类的，可以借鉴此类，以免重复
+
 * 路线规划函数Graph类中的journeySequence函数返回了一个Vector<Sight>，是一个按顺序排列的景点的向量，注意其中包含了酒店。
+
+
+
+
--- app/src/main/java/com/example/travelingagent/activity/LoginActivity.java
+++ app/src/main/java/com/example/travelingagent/activity/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.example.travelingagent.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
 import com.example.travelingagent.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     //public static final String EXTRA_USERNAME = "com.example.myfirstapp.MESSAGE";
@@ -14,6 +19,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        List<String> permissionList = new ArrayList<>();
+        permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        String [] permissions = permissionList.toArray(new String[permissionList.size()]);
+        ActivityCompat.requestPermissions(LoginActivity.this, permissions, 1);
     }
 
     /** Called when the user taps the Send button */
